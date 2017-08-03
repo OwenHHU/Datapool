2763ae5cdfaf0591657e64aa1a8874002b0b8d7f
jun.yu <jun.yu@datayes.com>
2017-08-03 08:54:49 +0800

    origin commit

---
+++ src/main/java/hpe/com/MeDB/App.java
@@ -0,0 +1,13 @@
+package hpe.com.MeDB;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
